e372dfa275881d4a5e52d62badc4915efc3d246a
Jayesh Kitukale <jayeshkitukale@Jayeshs-Mac-mini.local>
2015-04-28 14:11:59 +0530

    Initial Commit

---
+++ TrainMeFit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrainMeFit
+//
+//  Created by Jayesh Kitukale on 4/28/15.
+//  Copyright (c) 2015 Tungsten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
